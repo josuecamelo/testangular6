7a2e9353e30e9796164bf12a0113a8665145148c
Josué Camelo dos Santos Ferreira <josueprg@gmail.com>
2018-05-17 12:10:31 -0300

    descrição

--- src/app/my-service.service.ts
+++ src/app/my-service.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root' //já é registrado automagicamente, nao precisa adicionar no app.module.ts
+  providedIn: 'root' //já é registrado automagicamente, nao precisa adicionar no app.module.ts providers
 })
 export class MyServiceService {
   variavel = 'teste';
